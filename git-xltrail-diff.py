187baf2977ae4ad5bd42c3fb3a6ba077d3c624a2
Bjoern Stiel <bjoern.stiel@zoomeranalytics.com>
2018-02-03 12:18:06 +0000

    Fixed diff output and make it work for any arbitrary commit comparison

--- git-xltrail-diff.py
+++ git-xltrail-diff.py
@@ -36,41 +36,41 @@ def get_vba(workbook):
 
 
 if __name__ == '__main__':
-    _, workbook1, workbook2, _, _, _, _ , _ = sys.argv
-    
-    path_workbook1 = os.path.join(os.getcwd(), workbook1)
-    path_workbook2 = workbook2
-    
-    workbook1_modules = get_vba(path_workbook1)
-    workbook2_modules = get_vba(path_workbook2)
+    _, workbook_name, workbook_b, _, _, workbook_a, _ , _ = sys.argv
+
+    path_workbook_a = os.path.abspath(workbook_a)
+    path_workbook_b = os.path.abspath(workbook_b)
+
+    workbook_a_modules = get_vba(path_workbook_a)
+    workbook_b_modules = {} if workbook_b == 'nul' else get_vba(path_workbook_b)
 
     diffs = []
-    for module1, vba1 in workbook1_modules.items():
-        if module1 not in workbook2_modules:
-            diffs.append({
-                'a': '--- a/' + workbook1 + '/VBA/' + module1,
-                'b': '+++ /dev/null',
-                'diff': '\n'.join([Fore.RED + '-' + line for line in vba1.split('\n')])
-            })
-        elif vba1 != workbook2_modules[module1]:
-            diffs.append({
-                'a': '--- a/' + workbook1 + '/VBA/' + module1,
-                'b': '+++ b/' + workbook1 + '/VBA/' + module1,
-                'diff': '\n'.join([(Fore.RED if line.startswith('-') else (Fore.GREEN if line.startswith('+') else (Fore.CYAN if line.startswith('@') else ''))) + line.strip('\n') for line in list(unified_diff(workbook2_modules[module1].split('\n'), vba1.split('\n')))[2:]])
-            })
-
-    for module2, vba2 in workbook2_modules.items():
-        if module2 not in workbook1_modules:
+    for module_a, vba_a in workbook_a_modules.items():
+        if module_a not in workbook_b_modules:
             diffs.append({
                 'a': '--- /dev/null',
-                'b': '+++ b/' + workbook2 + '/VBA/' + module2,
-                'diff': '\n'.join(['+' + line for line in vba1.split('\n')])
+                'b': '+++ b/' + workbook_name + '/VBA/' + module_a,
+                'diff': '\n'.join([Fore.GREEN + '+' + line for line in vba_a.split('\n')])
+            })
+        elif vba_a != workbook_b_modules[module_a]:
+            diffs.append({
+                'a': '--- a/' + workbook_name + '/VBA/' + module_a,
+                'b': '+++ b/' + workbook_name + '/VBA/' + module_a,
+                'diff': '\n'.join([(Fore.RED if line.startswith('-') else (Fore.GREEN if line.startswith('+') else (Fore.CYAN if line.startswith('@') else ''))) + line.strip('\n') for line in list(unified_diff(workbook_b_modules[module_a].split('\n'), vba_a.split('\n')))[2:]])
+            })
+
+    for module_b, vba_b in workbook_b_modules.items():
+        if module_b not in workbook_a_modules:
+            diffs.append({
+                'a': '--- b/' + workbook_name + '/VBA/' + module_b,
+                'b': '+++ /dev/null',
+                'diff': '\n'.join([Fore.RED + '-' + line for line in vba_b.split('\n')])
             })
 
 
     colorama.init(strip=False)
 
-    print(Style.BRIGHT + 'diff --xltrail ' + 'a/' + workbook1 + ' b/' + workbook1)
+    print(Style.BRIGHT + 'diff --xltrail ' + 'a/' + workbook_name + ' b/' + workbook_name)
     for diff in diffs:
         print(Style.BRIGHT + diff['a'])
         print(Style.BRIGHT + diff['b'])
