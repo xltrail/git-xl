20341da118a2a6313062b3ba3c1cd18cc70d0d4a
Bjoern Stiel <bjoern.stiel@zoomeranalytics.com>
2018-02-06 20:23:13 +0000

    Update version info before build

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -2,7 +2,6 @@
 python -c "import sys;print('x64' if sys.maxsize > 2**32 else 'x86')" > PYTHON_ARCH
 set /p PYTHON_ARCH= < PYTHON_ARCH
 del PYTHON_ARCH
-ECHO python-%PYTHON_ARCH%.exe
 
 python .\scripts\windows\update-version-info.py
 pyinstaller --onefile .\git-xltrail-diff.py --name=git-xltrail-diff-%PYTHON_ARCH%.exe --version-file .\scripts\windows\git-xltrail-version-info.py --icon .\scripts\windows\git-xltrail-logo.ico
