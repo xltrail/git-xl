620132b24138e85a6596a5ac848555291f662272
Bjoern Stiel <bjoern.stiel@zoomeranalytics.com>
2018-02-07 22:51:54 +0000

    Some DRY

--- git-xltrail.py
+++ git-xltrail.py
@@ -42,14 +42,33 @@ class Installer:
 		self.mode = mode
 		self.path = path
 
+
 	def install(self):
+		# gitconfig: add diff.xltrail.command
 		self.execute(['diff.xltrail.command', GIT_XLTRAIL_DIFF])
-		git_attributes_path = self.git_attributes()
-		self.update_git_attributes(git_attributes_path)
+
+		# set gitattributes (define differ for Excel file formats)
+		git_attributes_path = self.get_git_attributes_path()
+		self.update_git_file(path=git_attributes_path, keys=GIT_ATTRIBUTES, operation='SET')
 
 		# set core.attributesfile when editing global git config
 		if self.mode == 'global':
-			self.execute(['core.attributesfile', global_attributes])
+			self.execute(['core.attributesfile', git_attributes_path])
+
+
+	def uninstall(self):
+		# remove diff.xltrail.command from gitconfig
+		keys = self.execute(['--list']).split('\n')
+		if [key for key in keys if key.startswith('diff.xltrail.command')]:
+			self.execute(['--remove-section', 'diff.xltrail'])
+
+		# remove keys from gitattributes
+		git_attributes_path = self.get_git_attributes_path()
+		keys = self.update_git_file(path=git_attributes_path, keys=GIT_ATTRIBUTES, operation='REMOVE')
+
+		# remove core.attributesfile when editing global git config and gitattributes is empty
+		if not keys and self.mode == 'global':
+			self.execute(['--remove-section', 'core.attributesfile'])
 
 
 	def execute(self, args):
@@ -59,7 +78,8 @@ class Installer:
 		command += args
 		return subprocess.run(command, cwd=self.path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout
 	
-	def git_attributes(self):
+
+	def get_git_attributes_path(self):
 		if self.mode == 'local':
 			return os.path.join(self.path, '.gitattributes')
 
@@ -74,40 +94,36 @@ class Installer:
 		p = self.execute(['--list']).split('\n')[0]
 		return f[:f.index(p)][5:][:-11] + '.gitattributes'
 
+
+	def get_git_ignore_path(self):
+		pass
 	
-	def update_git_attributes(self, path):
-		"""Update .gitattributes"""
+
+	def update_git_file(self, path, keys, operation):
+		assert operation in ('SET', 'REMOVE')
 		if os.path.exists(path):
 			with open(path, 'r') as f:
 				content = [line for line in f.read().split('\n') if line]
 		else:
 			content = []
-		content = sorted(list(set(content).union(set(GIT_ATTRIBUTES))))
-		with open(path, 'w') as f:
-			f.writelines('\n'.join(content))
+		
+		if operation == 'SET':
+			# create union set: keys + existing content
+			content = sorted(list(set(content).union(set(keys))))
+		else:
+			# remove keys from content
+			content = [line for line in content if line and line not in keys]
+	
+		if operation == 'REMOVE' and not content:
+			# no content
+			# delete file
+			os.remove(path)
+		else:
+			with open(path, 'w') as f:
+				f.writelines('\n'.join(content))
+		
+		return content
 
-
-
-	def uninstall(self):
-		keys = self.execute(['--list']).split('\n')
-		if [key for key in keys if key.startswith('diff.xltrail.command')]:
-			self.execute(['--remove-section', 'diff.xltrail'])
-
-		# clean up gitattributes
-		git_attributes = self.git_attributes()
-		if os.path.exists(git_attributes):
-			with open(git_attributes, 'r') as f:
-				content = f.read().split('\n')
-			content = [line for line in content if line and line not in GIT_ATTRIBUTES]
-			if not content:
-				# no content left
-				if self.mode == 'global':
-					self.execute(['--remove-section', 'core.attributesfile'])
-				# delete file
-				os.remove(git_attributes)
-			else:
-				with open(git_attributes, 'w') as f:
-					f.writelines('\n'.join(content))
 
 
 class CommandParser:
