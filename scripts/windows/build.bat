1be9038404718ff019955ecda5cf54a053673544
Bjoern Stiel <bjoern.stiel@zoomeranalytics.com>
2018-05-22 07:49:57 +0100

    Add merge exe to build process

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -5,4 +5,5 @@ del PYTHON_ARCH
 
 python .\scripts\windows\update-version-info.py
 pyinstaller --onefile .\git-xltrail-diff.py --name=git-xltrail-diff-%PYTHON_ARCH%.exe --version-file .\scripts\windows\git-xltrail-version-info.py --icon .\scripts\windows\git-xltrail-logo.ico
+pyinstaller --onefile .\git-xltrail-merge.py --name=git-xltrail-merge-%PYTHON_ARCH%.exe --version-file .\scripts\windows\git-xltrail-version-info.py --icon .\scripts\windows\git-xltrail-logo.ico
 pyinstaller --onefile .\git-xltrail.py --name=git-xltrail-%PYTHON_ARCH%.exe --version-file .\scripts\windows\git-xltrail-version-info.py --icon .\scripts\windows\git-xltrail-logo.ico
