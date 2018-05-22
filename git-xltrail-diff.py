9ccb93dfd1de0c3e8c5519c278db3f7b1c270dfc
Bjoern Stiel <bjoern.stiel@zoomeranalytics.com>
2018-05-22 22:17:37 +0100

    Align git xltrail ls-files and git xltrail differ output

--- git-xltrail-diff.py
+++ git-xltrail-diff.py
@@ -28,30 +28,30 @@ if __name__ == '__main__':
     workbook_a = Workbook(path_workbook_a)
     workbook_b = Workbook(path_workbook_b)
 
-    workbook_a_modules = dict([(m.name, m.content) for m in workbook_a.vba_modules])
-    workbook_b_modules = {} if workbook_b == 'nul' else dict([(m.name, m.content) for m in workbook_b.vba_modules])
+    workbook_a_modules = dict([(m.name, m) for m in workbook_a.vba_modules])
+    workbook_b_modules = {} if workbook_b == 'nul' else dict([(m.name, m) for m in workbook_b.vba_modules])
 
     diffs = []
     for module_a, vba_a in workbook_a_modules.items():
         if module_a not in workbook_b_modules:
             diffs.append({
                 'a': '--- /dev/null',
-                'b': '+++ b/' + workbook_name + '/VBA/' + module_a,
+                'b': '+++ b/' + workbook_name + '/VBA/' + vba_a.type + '/' + module_a,
                 'diff': '\n'.join([Fore.GREEN + '+' + line for line in vba_a.split('\n')])
             })
-        elif vba_a != workbook_b_modules[module_a]:
+        elif vba_a.digest != workbook_b_modules[module_a].digest:
             diffs.append({
-                'a': '--- a/' + workbook_name + '/VBA/' + module_a,
-                'b': '+++ b/' + workbook_name + '/VBA/' + module_a,
-                'diff': '\n'.join([(Fore.RED if line.startswith('-') else (Fore.GREEN if line.startswith('+') else (Fore.CYAN if line.startswith('@') else ''))) + line.strip('\n') for line in list(unified_diff(workbook_b_modules[module_a].split('\n'), vba_a.split('\n'), n=numlines))[2:]])
+                'a': '--- a/' + workbook_name + '/VBA/' + vba_a.type + '/' + module_a,
+                'b': '+++ b/' + workbook_name + '/VBA/' + vba_a.type + '/' + module_a,
+                'diff': '\n'.join([(Fore.RED if line.startswith('-') else (Fore.GREEN if line.startswith('+') else (Fore.CYAN if line.startswith('@') else ''))) + line.strip('\n') for line in list(unified_diff(workbook_b_modules[module_a].content.split('\n'), vba_a.content.split('\n'), n=numlines))[2:]])
             })
 
     for module_b, vba_b in workbook_b_modules.items():
         if module_b not in workbook_a_modules:
             diffs.append({
-                'a': '--- b/' + workbook_name + '/VBA/' + module_b,
+                'a': '--- b/' + workbook_name + '/VBA/' + vba_b.type + '/' + module_b,
                 'b': '+++ /dev/null',
-                'diff': '\n'.join([Fore.RED + '-' + line for line in vba_b.split('\n')])
+                'diff': '\n'.join([Fore.RED + '-' + line for line in vba_b.content.split('\n')])
             })
 
 
